debbf58a4a4214e57e6719d7b55dc77e00380de3
James Yu Cao <caoyu19920414@gmail.com>
2023-05-17 20:57:45 -0400

    Week9 Day3

--- Week9/Day3/02-data-on-elements/data.js
+++ Week9/Day3/02-data-on-elements/data.js
@@ -1,1 +1,20 @@
 // Your code here
+document.addEventListener("DOMContentLoaded", () => {
+
+  const form = document.getElementById("form");
+  const list = document.getElementById("shopping-list");
+
+  const eventHandler = (event) => {
+    const name = document.getElementById("name").value;
+    const type = document.getElementById("type").value;
+    const newListItem = document.createElement("li");
+    newListItem.innerText = `${type.toUpperCase()} | ${name.toUpperCase()}`;
+    newListItem.setAttribute("data-type", type);
+    list.appendChild(newListItem);
+    form.reset();
+    event.preventDefault();
+  }
+
+  form.addEventListener("submit", eventHandler);
+
+})
